cbcc02fcbe3b0195e8bd6ab3ef9dfd3fa000b749
iamyyasin <rainman1651@gmail.com>
2024-07-06 18:36:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const resMenuBtn = document.querySelector(".res-menu-btn");
+const resMenu = document.querySelector(".res-menu");
+
+resMenuBtn.addEventListener("click", () => {
+  resMenu.classList.toggle("show");
+});
